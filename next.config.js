57fae842af8ac62414bea2ed783618785b9aab1e
pks <pklab2>
2025-04-13 17:49:39 +0300

    bare minimum two

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    /* config options here */
+    output: "standalone",
+    eslint: {
+      ignoreDuringBuilds: true,
+    },
   images: {
     domains: ['assets.mwonya.com','mwonya-kasfa-assets-store.s3.us-east-1.amazonaws.com'],
   },
