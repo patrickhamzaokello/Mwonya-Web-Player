dc78950749d044bd4de2bdc3a8bca4df54f5b316
pks <pklab2>
2025-04-16 00:55:48 +0300

    update on player

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,7 @@ import {
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Mwonya Player",
+  title: "Mwonya Web Player",
   description: "Ugandan Number one Music and Video Platform",
 };
 
